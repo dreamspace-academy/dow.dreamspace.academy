55f3e36c7ff78dd8b7414c901c021d42ba394f2c
Kanapathipillaivinothraj <Kanapathipillaivinothraj@gmail.com>
2022-12-30 12:14:20 +0530

    chore: testing js DOM check index page

--- static/Script/JS/import.js
+++ static/Script/JS/import.js
@@ -1,4 +1,22 @@
+// home-content Dom
+const home_btn = document.getElementById('home')
+home_btn.onclick = Home;
+
+
+
+
+
 $(function () {
     $("#headerbar").load("./importFiles/navbar.html");
     $("#footerbar").load("./importFiles/footerbar.html");
 });
+
+
+
+
+
+function Home(params) {
+    $(function () {
+        $("#maincontent").load("./importFiles/homecontent.html");
+    });
+}
