39699f72417a8a970a682ab26c3b8a36000a0eb5
Kanapathipillaivinothraj <Kanapathipillaivinothraj@gmail.com>
2022-12-31 14:57:16 +0530

    chore: add new web etemplate

--- static/Script/JS/navbar.js
+++ static/Script/JS/navbar.js
@@ -1,21 +1,16 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
-
-
 //
 const home_btn = document.getElementById('home');
 home_btn.onclick = Home;
 const About_btn = document.getElementById('About');
 About_btn.onclick = About;
+
+
 function Home() {
   $(function () {
       $("#maincontent").load("./importFiles/homecontent.html");
   });
 };
+
 function About() {
   $(function () {
       $("#maincontent").load("./importFiles/Aboutcontent.html");
