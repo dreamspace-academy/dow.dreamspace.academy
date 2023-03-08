b8133d503c99e18d7922c1ebe8683379171de5d0
vinothraj <Kanapathipillaivinothraj@gmail.com>
2023-03-08 16:56:46 +0530

    fix: work on about page - change images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -109,11 +109,11 @@
 	})();
 
 
-	$(document).on("click", ".naccs .row div", function () {
+	$(document).on("click", ".naccs .menu div", function () {
 		var numberIndex = $(this).index();
 
 		if (!$(this).is("active")) {
-			$(".naccs .row div").removeClass("active");
+			$(".naccs .menu div").removeClass("active");
 			$(".naccs ul li").removeClass("active");
 
 			$(this).addClass("active");
